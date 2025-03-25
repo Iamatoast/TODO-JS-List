04875d9b36917d4a0b40dc60b23c96cf2e0317a8
SANTINO AGUILERA <48313116@ort>
2025-03-25 10:35:45 -0300

    Changed the way addListElement() gets the creationDate

--- code.js
+++ code.js
@@ -20,23 +20,25 @@ const refreshList = () => {
         else
         {
             list.innerHTML =
-            ``
-            listElements[i].text
-            listElements[i].creationDate
-            listElements[i].finishDate
-            ``    
+            `
+            <tr>
+                <th scope="row"><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></th>
+                <td>${listElements[i].text}</td>
+                <td>${listElements[i].creationDate}</td>
+                <td>${listElements[i].finishDate}</td>
+            </tr>
+            `
         }
     }
 }
 
 const addListElement = () => {
     let text = document.getElementById("text");
-    let creationDate = document.getElementById("creationDate");
 
     let listElement = {
         id: listElements.length,
         text: text,
-        creationDate: creationDate,
+        creationDate: Date.toDateString(),
         finishDate: null
     }
 
